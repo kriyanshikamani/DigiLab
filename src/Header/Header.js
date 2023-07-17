9cc2f0e4037e67504bfe5039714bf0e5734295f9
kriyanshikamani <kriyanshikamani@gmail.com>
2023-07-17 14:09:24 +0530

    Initializtion.

---
+++ src/Header/Header.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {BsCart3} from "react-icons/bs"
+
+const Header = () => {
+    const navbar=["Book Test/Packages","About Us","Download Report","Our Labs","Pay Online"]
+  return (
+    <div>
+        <nav className=' bg-gradient-to-l from-blue-900 to-blue-600 via-blue-700'>
+            <div className='relative w-[1080px] mx-auto flex justify-end'>
+                <ul className='flex space-x-6'>
+                
+                    {
+                        navbar.map((Btn,index)=>{
+                            return(
+                                <li key={index} className='text-white font-semibold text-md  py-7  hover:text-blue-300 cursor-pointer transition-all duration-200  relative group'>{Btn}
+                                <div className='absolute w-full h-1 bg-blue-200 bottom-0 hidden group-hover:block'></div>
+                                </li>
+                            )
+                        })
+                    }
+                    <span className='my-6 text-white hover:cursor-pointer text-2xl'><BsCart3 /></span>
+                    <div>
+                    <button className='rounded-sm my-5 w-20 h-9 text-white border-blue-300 border-2  font-bold text-md hover:text-blue-200 cursor-pointer'>Log in </button>
+                    <button className='rounded-sm  mx-4 my-5 w-20 h-9 text-blue-300 bg-white  font-bold text-md hover:text-blue-800 cursor-pointer'>Sign up </button>
+                    </div>
+                </ul>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Mainbody/Mainbody.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import drimage from '../Images/drimage.png';
+
+
+const Mainbody = () => {
+  return (
+    <div className='flex justify-end'>
+        <img src={drimage} alt="/" />
+    </div>
+  )
+}
+
+export default Mainbody
