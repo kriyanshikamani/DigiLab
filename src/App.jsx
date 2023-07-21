59266114fa2a0008ff52da959c0944f9d2dbf0c6
kriyanshikamani <kriyanshikamani@gmail.com>
2023-07-21 16:58:23 +0530

    Cimit and ...

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import Header from './components/Header/Header.jsx';
+import Mainbody from './components/Mainbody/Mainbody.jsx';
+import Footer from './components/Footer/Footer.jsx';
+import Test from './components/Test_Packages/Test.jsx';
+import './main.css';
+
+function App() {
+  return (
+    <>
+  
+      {/* <Header/>
+      <Mainbody/>
+      <Footer/> */}
+      <Test/>
+    </>
+  );
+}
+
+
+export default App
